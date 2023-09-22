81eea4dbdd73ebe39542037aa8e7793886a6d20b
Sanjeevi kumar <125111226+sanjiv-16@users.noreply.github.com>
2023-09-22 13:07:42 +0530

    Update User.cpp

--- Vending_Machine/User.cpp
+++ Vending_Machine/User.cpp
@@ -7,13 +7,19 @@ class User
 {
 private:
     double user_balance{};
+    User(){};
 
 public:
+    
     std::vector<Product> products;
-    User(){};
-    User(double entered_amount) { user_balance = entered_amount; }
+    static User create_user(){
+        return User();
+    }
     double get_user_balance() { return user_balance; }
     void set_user_balance(double balance) { user_balance = balance; }
+    void set_to_default(){
+        user_balance=0;
+    }
     
 };
 #endif
